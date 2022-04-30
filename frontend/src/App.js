863f70bf003e308507cfd00da351321e20bab075
mariem mkadem <mmkadem11@gmail.com>
2022-04-30 14:55:38 +0100

    add card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import {Container} from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 const App =()=> {
   return (
@@ -9,7 +10,7 @@ const App =()=> {
     <Header />
     <main className='py-3'>
       <Container>
-      <h1> Welcome to Proshop</h1>
+        <HomeScreen />
       </Container>
    </main>
    <Footer />

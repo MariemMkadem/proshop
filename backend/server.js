804dd006eba3e4bd7f1b658944c9079a2906848a
mariem mkadem <mmkadem11@gmail.com>
2022-05-01 22:32:18 +0100

    modeling data

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import products from './data/products.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors';
 
-const app = express();
+const app = express()
+dotenv.config()
+connectDB()
 
-dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.get('/',(req,res)=> {
-    res.send('api is running')
+app.get('/', (req, res) => {
+  res.send('api is running')
 })
 
-
-app.get('/api/products',(req,res)=> {
-    res.json(products);
+app.get('/api/products', (req, res) => {
+  res.json(products)
 })
 
-app.get('/api/products/:id',(req,res)=> {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product);
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
 })
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} noode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} noode on port ${PORT}`.yellow
+      .bold
+  )
+)

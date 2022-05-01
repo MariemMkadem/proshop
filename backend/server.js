484da002821bb27256b8ff15a36f7aef99e74afc
mariem mkadem <mmkadem11@gmail.com>
2022-05-01 10:23:40 +0100

    add env variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express' )
+const dotenv = require('dotenv')
 const products = require('./data/products')
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=> {
     res.send('api is running')
@@ -15,4 +19,4 @@ app.get('/api/products/:id',(req,res)=> {
     const product = products.find(p => p._id === req.params.id)
     res.json(product);
 })
-app.listen(5000, console.log('Server listen on port 5000'))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} noode on port ${PORT}`))

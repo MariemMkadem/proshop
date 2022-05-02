aef531a86eee734e0f5ea55154d89bd48d532a78
mariem mkadem <mmkadem11@gmail.com>
2022-05-02 01:06:29 +0100

    fetching products from db

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModal.js';
+
+const router = express.Router()
+
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+  })
+)
+
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({ message: 'Product not found' })
+    }
+  })
+)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
 
 const app = express()
 dotenv.config()
@@ -14,14 +14,8 @@ app.get('/', (req, res) => {
   res.send('api is running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
 app.listen(
   PORT,
   console.log(
